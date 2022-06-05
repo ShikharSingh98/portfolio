55cf378f80abda43c466b114f9dda1586fa8148f
ShikharSingh98 <shikharsingh98@gmail.com>
2022-06-05 20:35:42 +0530

    change navbar component

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-const navbarToggle = document.querySelector('#navbar-toggle');
-const navbar = document.querySelector('#navbar');
-
-navbarToggle.addEventListener('click', function () {
-  navbar.classList.toggle('show-navbar');
-});
-
 // ==================
 // Smooth Scrolling
 // ==================
