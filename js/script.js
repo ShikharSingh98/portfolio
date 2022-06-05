90d7d472b574638f10b34a4687522bc469388c20
ShikharSingh98 <shikharsingh98@gmail.com>
2022-06-05 19:33:09 +0530

    implemented smooth scrolling

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ const navbar = document.querySelector('#navbar');
 navbarToggle.addEventListener('click', function () {
   navbar.classList.toggle('show-navbar');
 });
+
+// ==================
+// Smooth Scrolling
+// ==================
+
+const allLinks = document.querySelectorAll('.navbar-link');
+
+allLinks.forEach((link) =>
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //scroll to top
+
+    if (href === '#home') window.scrollTo({ top: 0, behavior: 'smooth' });
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  })
+);
